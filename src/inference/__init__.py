9deed28b4c250144786736a523f4d3fa28c1c715
davidgonveg <davidgonveg@gmail.com>
2025-11-20 21:22:23 +0100

    Nuevo inicio: Limpieza de repositorio para estructura modular

--- src/inference/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Inference and prediction utilities.
-"""
-
-try:
-    from .predictor import create_notebook_submission
-    __all__ = ['create_notebook_submission']
-except ImportError:
-    __all__ = []
--- src/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Utility functions and helpers.
-"""
-
-try:
-    from .helpers import plot_training_curves
-    __all__ = ['plot_training_curves']
-except ImportError:
-    __all__ = []
